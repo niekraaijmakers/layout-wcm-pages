3bdd68cc800e77fd1efdce655aa1be47faef228e
Niek Raaijmakers <niek@adobe.com>
2019-03-26 19:34:12 +0100

    Removed unneeded invalidator / using more OOTB invalidators

--- core/src/main/java/com/adobe/examples/layoutwcm/core/httpcache/AutomatedLayoutPageInvalidator.java
+++ core/src/main/java/com/adobe/examples/layoutwcm/core/httpcache/AutomatedLayoutPageInvalidator.java
@@ -23,25 +23,33 @@ package com.adobe.examples.layoutwcm.core.httpcache;
 import com.adobe.acs.commons.httpcache.engine.HttpCacheEngine;
 import com.adobe.acs.commons.httpcache.exception.HttpCacheKeyCreationException;
 import com.adobe.acs.commons.httpcache.exception.HttpCachePersistenceException;
+import com.adobe.acs.commons.httpcache.invalidator.CacheInvalidationJobConstants;
 import com.adobe.examples.layoutwcm.core.httpcache.key.CookieKeyValueMapBuilder;
 import com.day.cq.commons.jcr.JcrConstants;
 import com.google.common.collect.ImmutableSet;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.resource.observation.ResourceChange;
 import org.apache.sling.api.resource.observation.ResourceChangeListener;
+import org.apache.sling.event.jobs.JobManager;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.ConfigurationPolicy;
 import org.osgi.service.component.annotations.Reference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static com.adobe.acs.commons.httpcache.invalidator.CacheInvalidationJobConstants.PAYLOAD_KEY_DATA_CHANGE_PATH;
+import static com.adobe.acs.commons.httpcache.invalidator.CacheInvalidationJobConstants.TOPIC_HTTP_CACHE_INVALIDATION_JOB;
 import static com.adobe.examples.layoutwcm.core.personalizedheader.Contants.USER_GROUP;
 import static com.adobe.examples.layoutwcm.core.personalizedheader.Contants.USER_GROUPS;
+import static java.util.Collections.singletonMap;
 
 /**
  * Automated Memory Cache Flusher
@@ -63,17 +71,12 @@ import static com.adobe.examples.layoutwcm.core.personalizedheader.Contants.USER
 public class AutomatedLayoutPageInvalidator implements ResourceChangeListener {
 
     private static final Logger LOG = LoggerFactory.getLogger(AutomatedLayoutPageInvalidator.class);
-    private static final String PATH_SUFFIX = "/" + JcrConstants.JCR_CONTENT + ".content.html";
 
     private static final Pattern PATTERN = Pattern.compile("((/content/layout-wcm/(headers|footers)/[a-zA-Z0-9_-]{1,99}))(.*)/(.*)");
-
-
+    
     @Reference
-    private HttpCacheEngine engine;
-
-    @Reference
-    private List<HeaderPageCacheExtension> headerPageCacheExtensions;
-
+    private JobManager jobManager;
+    
     @Override
     public void onChange(List<ResourceChange> changes) {
         for (ResourceChange change : changes) {
@@ -82,28 +85,15 @@ public class AutomatedLayoutPageInvalidator implements ResourceChangeListener {
             String layoutPagePath = extractHeaderPath(change.getPath());
 
             if (StringUtils.isNotEmpty(layoutPagePath)) {
-                LOG.debug("Extracted header path: {}", layoutPagePath);
-                try {
-                    LOG.debug("Flushing path {}", layoutPagePath + PATH_SUFFIX);
-                    engine.invalidateCache(layoutPagePath + PATH_SUFFIX);
-
-                    for(String userGroup :USER_GROUPS){
-                        String target = layoutPagePath + PATH_SUFFIX + String.format("[CookieKeyValues:%s=%s]", USER_GROUP, userGroup);
-                        engine.invalidateCache(target);
-                        //also for request keys specifically for dispatcher.
-                        engine.invalidateCache(target + "[HeaderKeyValues:Server-Agent=Communique-Dispatcher]");
-                    }
-
-                } catch (HttpCachePersistenceException | HttpCacheKeyCreationException e) {
-                    LOG.error("Error flushing path!", e);
-                }
+                LOG.debug("Flushing path {}", layoutPagePath);
+                jobManager.addJob(TOPIC_HTTP_CACHE_INVALIDATION_JOB, singletonMap(PAYLOAD_KEY_DATA_CHANGE_PATH,layoutPagePath));
             }
-
+            
         }
     }
 
-    private String extractHeaderPath(String headerChildResourcePath) {
-        Matcher matcher = PATTERN.matcher(headerChildResourcePath);
+    private String extractHeaderPath(String layoutPageChildResourcePath) {
+        Matcher matcher = PATTERN.matcher(layoutPageChildResourcePath);
 
         if (matcher.matches()) {
             return matcher.group(1);
