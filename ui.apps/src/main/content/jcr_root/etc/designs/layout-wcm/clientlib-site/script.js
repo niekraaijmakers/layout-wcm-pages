6d6290f460946733fbb44821e7442c67b1a7bfdd
Niek Raaijmakers <niek@adobe.com>
2019-08-13 18:57:36 +0200

    Added flyout demo + some basic JS / CSS.

--- ui.apps/src/main/content/jcr_root/etc/designs/layout-wcm/clientlib-site/script.js
+++ ui.apps/src/main/content/jcr_root/etc/designs/layout-wcm/clientlib-site/script.js
@@ -1,2 +1,32 @@
 /** a JS file that shall be included */
 
+$(document).ready(function(){
+
+    var $flyouts = $('.nav-flyout');
+    var currentlyActivePath = '';
+
+    $(document).click(function(event){
+        var $flyout = $(event.target).closest(".nav-flyout.active,.flyout-link");
+        if($flyout.length === 0){
+            $flyouts.removeClass('active');
+            currentlyActivePath = '';
+        }
+    });
+
+    $('.flyout-link').click(function(event){
+
+        event.preventDefault();
+
+        var targetPath = $(this).attr('href');
+        $flyouts.removeClass('active');
+
+        if(currentlyActivePath != targetPath){
+            $flyouts.filter('[data-flyout-path="' + targetPath + '"]').addClass('active');
+            currentlyActivePath = targetPath;
+        }else{
+            currentlyActivePath = '';
+        }
+    });
+
+
+});
